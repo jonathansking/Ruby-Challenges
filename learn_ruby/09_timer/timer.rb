3460996a18a2e0f1ad3318dc519caaa713bf009a
jsking <jsking@ucdavis.edu>
2015-07-19 20:33:39 -0700

    challenges 00, 01, 02, 03, 04, 08, 09 complete

---
+++ learn_ruby/09_timer/timer.rb
@@ -0,0 +1,15 @@
+class Timer
+  attr_accessor :seconds
+
+  def initialize(seconds=0)
+    @seconds = seconds
+  end
+
+  def time_string
+    hours = (@seconds / 3600).to_s.rjust(2, '0')
+    rem = @seconds % 3600
+    mins = (rem / 60).to_s.rjust(2, '0')
+    secs = (@seconds % 60).to_s.rjust(2, '0')
+    return hours + ":" + mins + ":" + secs
+  end
+end
